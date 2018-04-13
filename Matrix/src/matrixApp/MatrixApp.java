cafc643e9a43e58bcaf4e49780afb6ede20e4696
Joni Juntunen <Joni.M.Juntunen@student.lamk.fi>
2018-04-13 14:33:41 +0300

    Manage matrixes and matrix functions menu

--- Matrix/src/matrixApp/MatrixApp.java
+++ Matrix/src/matrixApp/MatrixApp.java
@@ -45,11 +45,52 @@ public class MatrixApp {
     
     private static void showMenu() {
         String menu = "MATRIX\n";
-        menu += "(C) Create new matrix\n";
-        menu += "(L) List matrixes\n";
-        menu += "(S) Sum two matrixes\n";
-        menu += "(Q) Quit\n";
+        menu += "(C). Create new matrix\n";
+        menu += "(L). List matrixes\n";
+        menu += "(S). Sum two matrixes\n";
+        menu += "(Q). Quit\n";
         System.out.println(menu);
+    }
+    
+    private static void showSelect() {
+        String menu = "Select matrix to manage\n";
+        menu += "(Q). Back to Main\n";
+        System.out.println(menu);
+    }
+    
+    private static void showManage(Matrix matrix) {
+        
+        String menu = "MATRIX - " + matrix + "\n";
+        menu += "(A). Add value\n";
+        menu += "(D). Delete value\n";
+        menu += "(M). Mulptiply values\n";
+        menu += "(P). Print\n";
+        menu += "(Q). Back to Matrix list\n";
+        System.out.println(menu);
+    }
+    
+    private static void manageMatrix(Matrix matrix) {
+        boolean run = true;
+        while(run) {
+            showManage(matrix);
+            
+            switch(System.console().readLine().toLowerCase()) {
+                case "a":
+                    matrix.set(2, 2, 3);
+                    break;
+                case "d":
+                    break;
+                case "m":
+                    matrix.product(2);
+                    break;
+                case "p":
+                    matrix.print();
+                    break;
+                case "q":
+                    run = false;
+                    break;
+            }
+        }
     }
     
     private static void add() {
@@ -57,9 +98,25 @@ public class MatrixApp {
     }
     
     private static void list() {
+       int i = 0;
        for (Matrix matrix: matrixList) {
-           System.out.println(matrix);
+           System.out.println("(" + i++ + "). " + matrix);
        }
+       while(true) {
+           showSelect();
+           String input = System.console().readLine().toLowerCase();
+           if (input.equals("q")) {
+               break;
+           }
+           try {
+               int id = Integer.parseUnsignedInt(input);
+               Matrix matrix = matrixList.get(id);
+               manageMatrix(matrix);
+           } catch (Exception ex) {
+               System.out.println("invalid id");
+           }
+       }
+       
     }
     
     private static void sum() {
