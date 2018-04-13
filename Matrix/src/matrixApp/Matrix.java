b4550f9ab2f3cee71742d9148227003bba53dc3f
Joni Juntunen <Joni.M.Juntunen@student.lamk.fi>
2018-04-13 12:19:17 +0300

    rename project matrix - matrixApp

---
+++ Matrix/src/matrixApp/Matrix.java
@@ -0,0 +1,18 @@
+package matrixApp;
+
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Joni, Sampo
+ */
+public class Matrix {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TreeMap<Point, Integer> values = new TreeMap<>();
+    }
+    
+}
